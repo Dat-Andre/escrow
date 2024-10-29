a967221a253c4959d455fcb684466180b1a9f8d8
Andre <dat.andre.on.juno@gmail.com>
2024-10-29 08:22:34 +0000

    add take

--- programs/escrow/src/instructions/make.rs
+++ programs/escrow/src/instructions/make.rs
@@ -40,14 +40,14 @@ pub struct Make<'info> {
 }
 
 impl<'info> Make<'info> {
-    pub fn init_escrow(&mut self, seed: u64, receive: u64, bumps: &MakeBumps) -> Result<()> {
+    pub fn init_escrow(&mut self, seed: u64, recieve: u64, bumps: &MakeBumps) -> Result<()> {
 
         self.escrow.set_inner(Escrow {
             seed,
             maker: self.maker.key(),
             mint_a: self.mint_a.key(),
             mint_b: self.mint_b.key(),
-            receive,
+            recieve,
             bump: bumps.escrow
         });
         Ok(())
